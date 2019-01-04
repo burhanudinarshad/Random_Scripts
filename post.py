52db4e0ac9a1fe85ccc3473a4e22874f25e147c2
burhanudinarshad <45567298+burhanudinarshad@users.noreply.github.com>
2019-01-04 17:56:26 +0500

    Update post.py

--- post.py
+++ post.py
@@ -1,22 +1,3 @@
 import urllib.request
 import json
 
-
-
-res = urllib.request.urlopen('https://picsum.photos/200/?random').read()
-#res_body = res.read()
-
-# https://docs.python.org/3/library/json.html
-#j = json.loads(res)
-
-print(res)
-
-
-res = urllib.request.urlopen('http://www.boredapi.com/api/activity/')
-res_body = res.read()
-
-# https://docs.python.org/3/library/json.html
-j = json.loads(res_body.decode("utf-8"))
-activity = j['activity']
-
-print(activity)
